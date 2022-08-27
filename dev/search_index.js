0d454dc9e1102e3007b9b60ca8274fa8da8c0536
Documenter.jl <documenter@juliadocs.github.io>
2022-08-27 19:07:26 +0000

    build based on 7489999

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = DEBmicroTrait","category":"page"},{"location":"#DEBmicroTrait","page":"Home","title":"DEBmicroTrait","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for DEBmicroTrait.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [DEBmicroTrait]","category":"page"}]
+}
