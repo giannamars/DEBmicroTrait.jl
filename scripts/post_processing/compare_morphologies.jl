9afa6bce8da02c9ccd6b4685945a2a51072f0d32
giannamars <giannamars@gmail.com>
2023-03-27 13:30:55 -0700

    update cell morphology

--- scripts/post_processing/compare_morphologies.jl
+++ scripts/post_processing/compare_morphologies.jl
@@ -1,12 +1,65 @@
 using DEBmicroTrait
-V_c   = [1e-18]
-N_p   = 10000*ones(1,1)
-rho_p = DEBmicroTrait.transporter_density(V_c, N_p)
-#rho_p = 0.01*ones(1,1)
-D_S   = [1e-10]
-K_0   = DEBmicroTrait.specific_reference_affinity(V_c, rho_p, D_S)
-min_gen_time = [4.0]
-N_SB  = DEBmicroTrait.transporter_density_to_monomer_uptake_sites(V_c, rho_p, min_gen_time, ["+"])
-Vmax  = 180.0*60^2 * N_SB
 
-affinity = Vmax./K_0
+# Interception probability
+N_p = 3140.0*ones(1,1)
+####################################################
+# Shape = sphere (r_c)
+r_c   = [1e-6]
+V_c_sphere   = @. (4/3)*π*r_c^3
+p_int_sphere = DEBmicroTrait.interception_probability(V_c_sphere, N_p)
+
+####################################################
+# Shape = spheroid (b~l)
+l_c   = [0.5e-6]
+b_c   = [0.25e-6]
+V_c_spheroid   = @. (4/3)*b_c^2*l_c
+p_int_spheroid = DEBmicroTrait.interception_probability(l_c, b_c, N_p)
+####################################################
+
+####################################################
+# Shape = stalk (b<<<l)
+l_c_stalk   = [0.5e-6]
+b_c_stalk   = [5e-10]
+V_c_stalk   = @. (4/3)*b_c^2*l_c
+p_int_stalk = DEBmicroTrait.interception_probability(l_c_stalk, b_c_stalk, N_p,"stalk")
+####################################################
+
+# Half-saturation constant
+D_S   = [1e-10]
+####################################################
+# Shape = sphere (r_c)
+rho_p = DEBmicroTrait.transporter_density(V_c_sphere, N_p)
+K_0_sphere   = DEBmicroTrait.specific_reference_affinity(V_c_sphere, rho_p, D_S)
+####################################################
+# Shape = spheroid (b~l)
+K_0_spheroid   = DEBmicroTrait.specific_reference_affinity(l_c, b_c, N_p, D_S)
+####################################################
+# Shape = stalk (b<<l)
+K_0_stalk   = DEBmicroTrait.specific_reference_affinity(l_c_stalk, b_c_stalk, N_p, D_S, "stalk")
+
+# Max. specific uptake rate
+min_gen_time = [4.0]
+gram_stain = ["+"]
+####################################################
+# Shape = sphere (r_c)
+N_SB_sphere  = DEBmicroTrait.transporter_density_to_monomer_uptake_sites(V_c_sphere, rho_p, min_gen_time, gram_stain)
+Vmax_sphere  = 180.0*60^2 * N_SB_sphere
+####################################################
+# Shape = spheroid (b~l)
+N_SB_spheroid  = DEBmicroTrait.transporter_density_to_monomer_uptake_sites(V_c_spheroid, N_p, min_gen_time, gram_stain, "spheroid")
+Vmax_spheroid  = 180.0*60^2 * N_SB_spheroid
+####################################################
+# Shape = stalk (b<<l)
+N_SB_stalk = DEBmicroTrait.transporter_density_to_monomer_uptake_sites(V_c_stalk, N_p, min_gen_time, gram_stain, "spheroid")
+Vmax_stalk  = 180.0*60^2 * N_SB_stalk
+
+# Affinity constant (S->0)
+####################################################
+# Shape = sphere (r_c)
+affinity_sphere = Vmax_sphere./K_0_sphere
+####################################################
+# Shape = spheroid (b~l)
+affinity_spheroid = Vmax_spheroid./K_0_spheroid
+####################################################
+# Shape = stalk (b<<l)
+affinity_stalk = Vmax_stalk./K_0_stalk
--- src/allometry/cell_assimilation.jl
+++ src/allometry/cell_assimilation.jl
@@ -11,6 +11,17 @@ function transporter_density_to_monomer_uptake_sites(V_c::Vector{Float64}, ρ_cl
     return N_SB
 end
 
+function transporter_density_to_monomer_uptake_sites(V_c::Vector{Float64}, N_p::Matrix{Float64}, Min_gen_time::Vector{Float64}, Gram_stain::Vector{String}, t::String)
+    gmax = log(2)./Min_gen_time
+    n_0 = cell_volume_to_cell_number_density(V_c, gmax, Gram_stain)
+    N_SB = zeros(size(N_p,1), size(N_p,2))
+    for i in 1:size(V_c,1)
+        N_SB[:,i] = n_0[i]*N_p[:,i]/12.011
+    end
+    N_SB[N_SB.<1e-12].=0.0
+    return N_SB
+end
+
 function transporters_closure(ρ_p::Vector{Float64}, genome_distr::Matrix{Float64})
     closure   = zeros(size(genome_distr))
     for i in 1:size(closure,2)
@@ -28,6 +39,53 @@ function transporter_density(V_c::Array{Float64,1}, N_p::Array{Float64,2})
         ρ_p[:,i] = N_p[:,i]*r_p^2/(4*r_c[i]^2)
     end
     return ρ_p
+end
+
+
+function interception_probability(V_c::Array{Float64,1}, N_p::Array{Float64,2})
+    r_p = 1e-9                    
+    r_c = (3*V_c/(4*pi)).^(1/3)
+    p_int = zeros(size(N_p,1), size(N_p,2))
+    for i in 1:size(V_c,1)
+        p_int[:,i] = @. (N_p[:,i]*r_p[i])/(N_p[:,i]*r_p + π*r_c[i])
+    end
+    return p_int
+end
+
+function interception_probability(l_c::Array{Float64,1}, b_c::Array{Float64,1}, N_p::Array{Float64,2})
+    r_p = 1e-9
+    A_c = @. 2π*b_c*(b_c+l_c)
+    
+    β_c = zeros(size(N_p,1), size(N_p,2))
+    for i in 1:size(l_c,1)
+        β_c[:,i] = @. 1 - N_p[:,i]*π*r_p^2/A_c[i]
+    end
+
+    P_s = @. 1 - 2b_c*r_p/(l_c*(b_c + l_c))
+
+    p_int = zeros(size(N_p,1), size(N_p,2))
+    for i in 1:size(l_c,1)
+        p_int[:,i] =  @. (1-β_c[:,i])*P_s[i]/(1-β_c[:,i]*P_s[i])
+    end
+    return p_int
+end
+
+function interception_probability(l_c::Array{Float64,1}, b_c::Array{Float64,1}, N_p::Array{Float64,2}, t::String)
+    r_p = 1e-9
+    A_c = @. π^2*l_c*b_c
+    
+    β_c = zeros(size(N_p,1), size(N_p,2))
+    for i in 1:size(l_c,1)
+        β_c[:,i] = @. 1 - N_p[:,i]*π*r_p^2/A_c[i]
+    end
+
+    P_s = @. 1 - 4r_p/(π*b_c)*1/log(2l_c/b_c)
+
+    p_int = zeros(size(N_p,1), size(N_p,2))
+    for i in 1:size(l_c,1)
+        p_int[:,i] =  @. (1-β_c[:,i])*P_s[i]/(1-β_c[:,i]*P_s[i])
+    end
+    return p_int
 end
 
 
@@ -50,3 +108,43 @@ function specific_reference_affinity(V_c::Vector{Float64}, ρ_p::Matrix{Float64}
     #
     any(x->x==true, isnan.(K_SC_0)) ? throw(DomainError("NaN in DEBmicroTrait.ECA_kinetics!"))  : return K_SC_0.*10
 end
+
+function specific_reference_affinity(l_c::Array{Float64,1}, b_c::Array{Float64,1}, N_p::Matrix{Float64}, D_S::Vector{Float64})
+    # N_p (number of transporters) -> ρ_p = N_p*4*pi*r_p^2/(4*pi*r_c^2) (transporter density on the cell surface)
+    V_S = 180.0                   # k2p, [s]
+    N_A = 6.022e23                # Avogrado constant [1/mol]
+    #
+    p_inv = (interception_probability(l_c, b_c, N_p))^(-1)
+    #
+    e_c   = @. (1-b_c^2/l_c^2)^(1/2)
+    l_eff = @. (e_c/tanh(e_c)^(-1))*l_c
+    I_max = @. 4π*D_S*l_eff
+    #
+    K_SC_0 = zeros(size(N_p,1), size(N_p,2))
+    for i in 1:size(N_p,2)
+        K_SC_0[:,i] = @. ((V_S*N_p[:,i])/(I_max[i]*N_A))*p_inv[:,i]
+    end
+    #
+    any(x->x==true, isnan.(K_SC_0)) ? throw(DomainError("NaN in DEBmicroTrait.ECA_kinetics!"))  : return K_SC_0.*10
+end
+
+function specific_reference_affinity(l_c::Array{Float64,1}, b_c::Array{Float64,1}, N_p::Matrix{Float64}, D_S::Vector{Float64}, t::String)
+    # N_p (number of transporters) -> ρ_p = N_p*4*pi*r_p^2/(4*pi*r_c^2) (transporter density on the cell surface)
+    V_S = 180.0                   # k2p, [s]
+    N_A = 6.022e23                # Avogrado constant [1/mol]
+    #
+    p_inv = (interception_probability(l_c, b_c, N_p, t))^(-1)
+    #
+    e_c   = @. (1-b_c^2/l_c^2)^(1/2)
+    l_eff = @. (e_c/tanh(e_c)^(-1))*l_c
+    I_max = @. 4π*D_S*l_eff
+    #
+    K_SC_0 = zeros(size(N_p,1), size(N_p,2))
+    for i in 1:size(N_p,2)
+        K_SC_0[:,i] = @. ((V_S*N_p[:,i])/(I_max[i]*N_A))*p_inv[:,i]
+    end
+    #
+    any(x->x==true, isnan.(K_SC_0)) ? throw(DomainError("NaN in DEBmicroTrait.ECA_kinetics!"))  : return K_SC_0.*10
+end
+
+
